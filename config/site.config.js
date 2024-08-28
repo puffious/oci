508ec19b72f734276c2225f60756284e730fa690
puffious <59928150+puffious@users.noreply.github.com>
2024-08-29 02:11:51 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -45,7 +45,7 @@ module.exports = {
   protectedRoutes: ['/Pious/Private','/Misc/Videos','/Pious/School'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'eightpuff@proton.me',
+  email: '',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
